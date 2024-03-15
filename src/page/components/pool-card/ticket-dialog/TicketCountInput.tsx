1a7da890e27f47a966f6dd37d3bd0a9cb2020241
sparqxi <146112627+sparqxi@users.noreply.github.com>
2024-03-14 21:47:27 -0700

    dialog

--- src/page/components/pool-card/ticket-dialog/TicketCountInput.tsx
+++ src/page/components/pool-card/ticket-dialog/TicketCountInput.tsx
@@ -7,13 +7,15 @@ const Root = styled(Box)(({theme}) => ({
     borderRadius:20,
     backgroundColor:theme.palette.mode ==='dark'?theme.palette.grey[700]:"#fff",
     display:'flex',
-    padding:8
+    padding:"4px 16px",
+    marginTop:8
 }))
 
 const TicketCountInput = (props: Props) => {
   return (
     <Root>
         <img src="assets/icon/ton.svg" alt="ton" style={{marginRight:'8px'}}/>
+        {/* TODO: increment number */}
         <InputBase type='number' fullWidth/>
         <Box sx={{ml:2}}>
             <img src="assets/icon/up.svg" alt="up" style={{cursor:'pointer'}}/>
--- src/page/components/pool-card/ticket-dialog/index.tsx
+++ src/page/components/pool-card/ticket-dialog/index.tsx
@@ -1,21 +1,59 @@
-import { Box, Dialog, styled } from '@mui/material'
-import React from 'react'
-import TicketCountInput from './TicketCountInput'
+import {
+  Box,
+  Button,
+  Dialog,
+  Divider,
+  Grid,
+  Typography,
+  styled,
+} from "@mui/material";
+import React from "react";
+import TicketCountInput from "./TicketCountInput";
 
 type Props = {
-    open:boolean
-    handleClose : () =>void
-}
+  open: boolean;
+  handleClose: () => void;
+};
 
-
-
-const TicketDialog = ({open,handleClose}: Props) => {
+const TicketDialog = ({ open, handleClose }: Props) => {
+  // TODO: background of dialog
   return (
-    <Dialog open={open} PaperProps={{sx:{padding:'18px',borderRadius:8,backgroundColor:'#F9FAFE'}}} onClose={handleClose} fullWidth maxWidth="xs">
-        <img src='assets/icon/close 1.svg' alt="close" style ={{position:'absolute',top:'20px',right:'30px'}}/>
-        <TicketCountInput/>
+    <Dialog
+      open={open}
+      PaperProps={{
+        sx: { padding: 6, borderRadius: 8, backgroundColor: "#F9FAFE" },
+      }}
+      onClose={handleClose}
+      fullWidth
+      maxWidth="xs"
+    >
+      <img
+        src="assets/icon/close 1.svg"
+        alt="close"
+        style={{ position: "absolute", top: "20px", right: "30px" }}
+      />
+      <TicketCountInput />
+      <Grid container alignItems="center" mt={2}>
+        <Typography color={"text.disabled"}>Ticket price</Typography>
+        <Grid item flex={1}>
+          <Divider orientation="horizontal" variant="middle" flexItem />
+        </Grid>
+        <Typography>0.5 TON</Typography>
+      </Grid>
+      <Grid container alignItems="center" mt={2}>
+        <Typography color={"text.disabled"}>Total price</Typography>
+        <Grid item flex={1}>
+          <Divider orientation="horizontal" variant="middle" flexItem />
+        </Grid>
+        <Typography>9 TON</Typography>
+      </Grid>
+      <Grid container justifyContent="center" mt={4}>
+        <Button variant="contained" color="secondary">
+          Get ticket
+        </Button>
+      </Grid>
     </Dialog>
-  )
-}
+  );
+};
 
-export default TicketDialog
+export default TicketDialog;
